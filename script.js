8d0e561060d42a29241d91309c20dc4b0b94da4a
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2024-03-12 20:33:21 -0300

    Versão aprimorada

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const prova1 = Vue.createApp({
-    //Um display vazio e dois vetores para carregar os numeros e operadores matematicos mostrados em tela
+    // Estrutura oque sera mostrado na tela da calculadora, os dois vetores serão varridos no codigo html
     data() {
         return {
             display: '',
@@ -9,67 +9,63 @@ const prova1 = Vue.createApp({
     },
     methods: {
 
-        //Tela da calculadora que começa vazia e recebe um numero pelo input do usuario
+        //Adiciona o input a tela da calculadora
         calculadoraTela(num) {
             this.display += num;
         },
 
-        //Função sera carregada pelo evento e pelo vetor de operadores matematicos pré estabelecidos
+        //Adiciona o operador a tela da calculadora e certifica que é um operador
         operacao(operador) {
-            if (this.display !== '' && !isNaN(parseFloat(this.display))) {
+            const lastChar = this.display.slice(-1);
+            if (lastChar && !this.operadores.includes(lastChar)) {
                 this.display += operador;
             }
         },
-
-        //Chama a função para executar as operações e mostrar na tela da calculadora, criada com try/catch para tratatamento de erro
-        //Caso o resultado obtido da função executarOpracoes nao satisfazer os criterios, ele mostrara a mensagem Erro no display da calculadora
+        //Função que irá começar o calculo, passara oque esta na tela para a proxina função caso o TRY esteja todo correto
+        //Se alguma validação falhar, caira no CATCH onde mostrará mensagem de erro
         calcular() {
             try {
                 const resultado = this.executarOperacoes(this.display);
-                this.display = resultado;
+                this.display = resultado.toString();
             } catch (error) {
                 this.display = 'Erro';
             }
         },
-
-        //Após chamada pela função calcular inicia a logica das operações de acordo com o operador matematico recebido pelo evento
-        //Foi adicionada REGEX para um codigo mais correto, usando do metodo TEST para certificar que sera retornado o padrão pedido
-        //OBS Fui auxiliado para a criação das expressões regulares
+        //Função chamada pela anterior, recebera a expressão que esta na tela 
+        //Os dados serão estruturados de acordo com oque a expressão regelular aceitar, efetuando os calculos e retornando para a função anterior o resultado
         executarOperacoes(expressao) {
             const operacoes = expressao.match(/(\d+|\+|\-|\*|\/)/g);
-            let resultado = 0;
-            let operador = '+';
+            let resultado = parseFloat(operacoes[0]);
 
-            //Nesse for in sera checado se o ultimo digito é um numero, caso não for, ele checara qual operador foi informado e executara a operação
-            for (const num of operacoes) {
-                if (/^\d+$/.test(num)) {
-                    switch (operador) {
-                        case '+':
-                            resultado += parseFloat(num);
-                            break;
-                        case '-':
-                            resultado -= parseFloat(num);
-                            break;
-                        case '*':
-                            resultado *= parseFloat(num);
-                            break;
-                        case '/':
-                            resultado /= parseFloat(num);
-                            break;
-                    }
-                } else {
-                    operador = num;
+            for (let i = 1; i < operacoes.length; i += 2) {
+                const operador = operacoes[i];
+                const numero = parseFloat(operacoes[i + 1]);
+
+                //Irá receber o operador e ativara a condição necessaria para a execução da função
+                switch (operador) {
+                    case '+':
+                        resultado += numero;
+                        break;
+                    case '-':
+                        resultado -= numero;
+                        break;
+                    case '*':
+                        resultado *= numero;
+                        break;
+                    case '/':
+                        if (numero === 0) throw 'Erro';
+                        resultado /= numero;
+                        break;
                 }
             }
 
             return resultado;
         },
 
-        //Limpa o display da calculadora
+        //Função para limpar a tela da calculadora 
         limpar() {
             this.display = '';
         }
-
     }
 });
 
